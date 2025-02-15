5dca17e0ee7badfa288d71a26e2dd372549fb56e
Night-stars-1 <nujj1042633805@gmail.com>
2025-02-16 02:29:02 +0800

    feat: 添加折叠框折叠动画

--- app/src/main/java/com/srap/nga/ui/component/card/ExpandableCard.kt
+++ app/src/main/java/com/srap/nga/ui/component/card/ExpandableCard.kt
@@ -1,11 +1,21 @@
 package com.srap.nga.ui.component.card
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -22,20 +32,35 @@ fun ExpandableCard(
 ) {
     var expanded by remember { mutableStateOf (false) }
 
-    Card(
-        shape = RoundedCornerShape(8.dp),
+    Box(
         modifier = Modifier
-            .fillMaxSize()
             .padding(bottom = 2.dp)
-            .clickable {
-                expanded = !expanded
-            }
+            .background(
+                MaterialTheme.colorScheme.surfaceContainer,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .animateContentSize()
+            .fillMaxWidth()
+            .clickable { expanded = !expanded }
     ) {
         Column {
-            Text(
-                text = title,
-                modifier = Modifier.padding(8.dp)
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = title,
+                    modifier = Modifier.padding(8.dp)
+                )
+
+                IconButton(onClick = { expanded = !expanded }) {
+                    if (expanded) {
+                        Icon(Icons.Default.KeyboardArrowUp, contentDescription = "展开")
+                    } else {
+                        Icon(Icons.Default.KeyboardArrowDown, contentDescription = "折叠")
+                    }
+                }
+            }
             if (expanded) {
                 content()
             }
