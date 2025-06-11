89c881f32baecb76becf76a89eacb4128c586ed7
Night-stars-1 <nujj1042633805@gmail.com>
2025-06-11 17:43:44 +0800

    chore: 提升版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.srap.nga"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
+        versionCode = 8
         versionName = "测试版"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
